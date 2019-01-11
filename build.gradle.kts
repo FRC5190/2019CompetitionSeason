15110e51ea3ea682c224c6f562dbb8c28c14d168
ThePrimedTNT <13050203+ThePrimedTNT@users.noreply.github.com>
2019-01-11 15:23:08 -0500

    Add VisionAssistedTrajectory

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,7 +81,7 @@ dependencies {
     compile("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.1.0")
 
     // FalconLibrary
-    compile("org.ghrobotics", "FalconLibrary", "abbab11")
+    compile("org.ghrobotics", "FalconLibrary", "22c7ee9")
 
     // WPILib and Vendors
     wpi.deps.wpilib().forEach { compile(it) }
