c731763612bd0bbaab5e081538999f58f7bd1bc7
Prateek Machiraju <prateek.machiraju@gmail.com>
2019-01-17 10:36:02 -0500

    Add some Talon SRX and LED methods

--- src/main/kotlin/org/ghrobotics/frc2019/Controls.kt
+++ src/main/kotlin/org/ghrobotics/frc2019/Controls.kt
@@ -13,28 +13,34 @@ import org.ghrobotics.lib.wrappers.hid.*
 
 object Controls {
 
+    var isClimbing = false
+        private set
+
     val mainXbox = xboxController(0) {
+        state({ !isClimbing }) {
+            // Align with Vision Target
+            triggerAxisButton(GenericHID.Hand.kLeft).change(VisionDriveCommand())
 
-        // Align with Vision Target
-        triggerAxisButton(GenericHID.Hand.kLeft).change(VisionDriveCommand())
+            // Emergency Mode
+            button(kBack).changeOn {
+                Robot.emergencyReadySystems.forEach { system -> system.activateEmergency() }
+                Robot.emergencyActive = true
+            }
+            button(kStart).changeOn {
+                Robot.emergencyReadySystems.forEach { system -> system.recoverFromEmergency() }
+                Robot.emergencyActive = false
+            }
 
-        // Emergency Mode
-        button(kBack).changeOn {
-            Robot.emergencyReadySystems.forEach { system -> system.activateEmergency() }
+            // Elevator Controls
+            val elevatorUpCommand = OpenLoopElevatorCommand(0.4)
+            val elevatorDownCommand = OpenLoopElevatorCommand(-0.4)
+
+            triggerAxisButton(GenericHID.Hand.kRight, 0.2).change(elevatorUpCommand)
+            button(kBumperRight).change(elevatorDownCommand)
+
+            button(kA).changeOn { DriveSubsystem.lowGear = true }
+            button(kA).changeOff { DriveSubsystem.lowGear = false }
         }
-        button(kStart).changeOn {
-            Robot.emergencyReadySystems.forEach { system -> system.recoverFromEmergency() }
-        }
-
-        // Elevator Controls
-        val elevatorUpCommand = OpenLoopElevatorCommand(0.4)
-        val elevatorDownCommand = OpenLoopElevatorCommand(-0.4)
-
-        triggerAxisButton(GenericHID.Hand.kRight, 0.2).change(elevatorUpCommand)
-        button(kBumperRight).change(elevatorDownCommand)
-
-        button(kA).changeOn { DriveSubsystem.lowGear = true }
-        button(kA).changeOff { DriveSubsystem.lowGear = false }
     }
 
     fun update() {
