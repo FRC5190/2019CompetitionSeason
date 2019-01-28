152cf704926b632bcbb5626af29c927dc0843b77
ThePrimedTNT <13050203+ThePrimedTNT@users.noreply.github.com>
2019-01-27 20:34:41 -0500

    Bound Arm Native Units

--- src/main/kotlin/org/ghrobotics/frc2019/subsystems/arm/ArmNativeUnitModel.kt
+++ src/main/kotlin/org/ghrobotics/frc2019/subsystems/arm/ArmNativeUnitModel.kt
@@ -3,36 +3,44 @@ package org.ghrobotics.frc2019.subsystems.arm
 import org.ghrobotics.lib.mathematics.units.Rotation2d
 import org.ghrobotics.lib.mathematics.units.nativeunits.NativeUnit
 import org.ghrobotics.lib.mathematics.units.nativeunits.NativeUnitModel
+import kotlin.math.withSign
 
 class ArmNativeUnitModel(
     armOffsetNativeUnits: NativeUnit,
     armOffsetAngle: Rotation2d,
-    sensorUnitsPerRotation: NativeUnit
+    sensorResolution: NativeUnit
 ) : NativeUnitModel<Rotation2d>(Rotation2d(0.0)) {
 
     private val armOffsetNativeUnits = armOffsetNativeUnits.value
     private val armOffsetAngle = armOffsetAngle.radian
-    private val nativeUnitsPerRotation = sensorUnitsPerRotation.value
+    private val sensorResolution = sensorResolution.value
 
     override fun fromNativeUnitPosition(nativeUnits: Double): Double {
-        val valueFromOffset = nativeUnits - armOffsetNativeUnits
-        val angleFromOffset = 2.0 * Math.PI * (valueFromOffset / nativeUnitsPerRotation)
+        val valueFromOffset = boundNativeUnit(nativeUnits - armOffsetNativeUnits)
+        val angleFromOffset = 2.0 * Math.PI * (valueFromOffset / sensorResolution)
         return angleFromOffset + armOffsetAngle
     }
 
     override fun toNativeUnitPosition(modelledUnit: Double): Double {
         val valueFromOffset = modelledUnit - armOffsetAngle
-        val nativeUnitsFromOffset = (valueFromOffset / (2.0 * Math.PI)) * nativeUnitsPerRotation
-        return nativeUnitsFromOffset + armOffsetNativeUnits
+        val nativeUnitsFromOffset = (valueFromOffset / (2.0 * Math.PI)) * sensorResolution
+        return boundNativeUnit(nativeUnitsFromOffset + armOffsetNativeUnits)
+    }
+
+    private fun boundNativeUnit(nativeUnit: Double): Double {
+        var boundNativeUnit = nativeUnit
+        while(boundNativeUnit < 0) boundNativeUnit += sensorResolution
+        while(boundNativeUnit > sensorResolution) boundNativeUnit -= sensorResolution
+        return boundNativeUnit.withSign(armOffsetNativeUnits)
     }
 
     override fun toNativeUnitError(modelledUnit: Double): Double =
-        (modelledUnit / (Math.PI * 2.0)) * nativeUnitsPerRotation
+        (modelledUnit / (Math.PI * 2.0)) * sensorResolution
 
     override fun fromNativeUnitVelocity(nativeUnitVelocity: Double): Double =
-        (nativeUnitVelocity / nativeUnitsPerRotation) * (Math.PI * 2.0)
+        (nativeUnitVelocity / sensorResolution) * (Math.PI * 2.0)
 
     override fun toNativeUnitVelocity(modelledUnitVelocity: Double): Double =
-        (modelledUnitVelocity / (Math.PI * 2.0)) * nativeUnitsPerRotation
+        (modelledUnitVelocity / (Math.PI * 2.0)) * sensorResolution
 
 }
--- src/test/kotlin/org/ghrobotics/frc2019/subsystems/arm/ArmNativeUnitModelTest.kt
+++ src/test/kotlin/org/ghrobotics/frc2019/subsystems/arm/ArmNativeUnitModelTest.kt
@@ -11,28 +11,34 @@ class ArmNativeUnitModelTest {
     private val model1 = ArmNativeUnitModel(
         armOffsetNativeUnits = 512.STU,
         armOffsetAngle = 90.degree,
-        sensorUnitsPerRotation = 1024.STU
+        sensorResolution = 1024.STU
     )
 
     private val model2 = ArmNativeUnitModel(
-        armOffsetNativeUnits = 500.STU,
+        armOffsetNativeUnits = (-512).STU,
         armOffsetAngle = 90.degree,
-        sensorUnitsPerRotation = 1024.STU
+        sensorResolution = 1024.STU
     )
 
     @Test
     fun testToNativeUnit1() {
-        val one = 0.degree
-        val two = 90.degree
-        val three = 180.degree
+        val one = (-225).degree
+        val two = (-180).degree
+        val three = 0.degree
+        val four = 180.degree
+        val five = 225.degree
 
         val oneNativeUnit = model1.toNativeUnitPosition(one)
         val twoNativeUnit = model1.toNativeUnitPosition(two)
         val threeNativeUnit = model1.toNativeUnitPosition(three)
+        val fourNativeUnit = model1.toNativeUnitPosition(four)
+        val fiveNativeUnit = model1.toNativeUnitPosition(five)
 
-        assert(oneNativeUnit.value epsilonEquals 256.0)
-        assert(twoNativeUnit.value epsilonEquals 512.0)
-        assert(threeNativeUnit.value epsilonEquals 768.0)
+        assert(oneNativeUnit.value epsilonEquals 640.0)
+        assert(twoNativeUnit.value epsilonEquals 768.0)
+        assert(threeNativeUnit.value epsilonEquals 256.0)
+        assert(fourNativeUnit.value epsilonEquals 768.0)
+        assert(fiveNativeUnit.value epsilonEquals 896.0)
     }
 
     @Test
@@ -52,17 +58,23 @@ class ArmNativeUnitModelTest {
 
     @Test
     fun testToNativeUnit2() {
-        val one = 0.degree
-        val two = 90.degree
-        val three = 180.degree
+        val one = (-225).degree
+        val two = (-180).degree
+        val three = 0.degree
+        val four = 180.degree
+        val five = 225.degree
 
         val oneNativeUnit = model2.toNativeUnitPosition(one)
         val twoNativeUnit = model2.toNativeUnitPosition(two)
         val threeNativeUnit = model2.toNativeUnitPosition(three)
+        val fourNativeUnit = model2.toNativeUnitPosition(four)
+        val fiveNativeUnit = model2.toNativeUnitPosition(five)
 
-        assert(oneNativeUnit.value epsilonEquals 244.0)
-        assert(twoNativeUnit.value epsilonEquals 500.0)
-        assert(threeNativeUnit.value epsilonEquals 756.0)
+        assert(oneNativeUnit.value epsilonEquals -640.0)
+        assert(twoNativeUnit.value epsilonEquals -768.0)
+        assert(threeNativeUnit.value epsilonEquals -256.0)
+        assert(fourNativeUnit.value epsilonEquals -768.0)
+        assert(fiveNativeUnit.value epsilonEquals -896.0)
     }
 
     @Test
@@ -97,17 +109,20 @@ class ArmNativeUnitModelTest {
 
     @Test
     fun testFromNativeUnit2() {
-        val one = 244.STU
-        val two = 500.STU
-        val three = 756.STU
+        val one = ( -640).STU
+        val two = (-768).STU
+        val three = (-256).STU
+        val four = (-896.0).STU
 
         val oneAngle = model2.fromNativeUnitPosition(one)
         val twoAngle = model2.fromNativeUnitPosition(two)
         val threeAngle = model2.fromNativeUnitPosition(three)
+        val fourAngle = model2.fromNativeUnitPosition(four)
 
-        assert(oneAngle.degree epsilonEquals 0.0)
-        assert(twoAngle.degree epsilonEquals 90.0)
-        assert(threeAngle.degree epsilonEquals 180.0)
+        assert(oneAngle epsilonEquals (-225).degree)
+        assert(twoAngle epsilonEquals (-180).degree)
+        assert(threeAngle epsilonEquals 0.degree)
+        assert(fourAngle epsilonEquals 225.degree)
     }
 
 }
