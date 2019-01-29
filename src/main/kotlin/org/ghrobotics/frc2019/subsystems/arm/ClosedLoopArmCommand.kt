22c1c0b3427e0a359c274b5f6400c61952659fd8
5190laptop <ghhscomputer@gmail.com>
2019-01-28 20:45:01 -0500

    Add back finish condition

--- src/main/kotlin/org/ghrobotics/frc2019/subsystems/arm/ClosedLoopArmCommand.kt
+++ src/main/kotlin/org/ghrobotics/frc2019/subsystems/arm/ClosedLoopArmCommand.kt
@@ -1,10 +1,16 @@
 package org.ghrobotics.frc2019.subsystems.arm
 
+import org.ghrobotics.frc2019.Constants
 import org.ghrobotics.lib.commands.FalconCommand
 import org.ghrobotics.lib.mathematics.units.Rotation2d
 
 class ClosedLoopArmCommand(private val target: Rotation2d) : FalconCommand(ArmSubsystem) {
-
+    init {
+        finishCondition += {
+            (ArmSubsystem.armPosition - target).absoluteValue < Constants.kArmClosedLoopTolerance &&
+                ArmSubsystem.velocity.absoluteValue < Constants.kArmClosedLoopVelocityTolerance
+        }
+    }
 
     override suspend fun initialize() {
         ArmSubsystem.armPosition = target
