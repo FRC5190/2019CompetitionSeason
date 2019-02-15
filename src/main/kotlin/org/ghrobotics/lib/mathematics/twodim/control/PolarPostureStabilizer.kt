74c9483a6ca7a42926a04bd925ffaa357d566e9f
Prateek Machiraju <prateek.machiraju@gmail.com>
2019-02-15 17:13:01 -0500

    Finish polar posture stabilizer

--- src/main/kotlin/org/ghrobotics/lib/mathematics/twodim/control/PolarPostureStabilizer.kt
+++ src/main/kotlin/org/ghrobotics/lib/mathematics/twodim/control/PolarPostureStabilizer.kt
@@ -1,13 +1,71 @@
 package org.ghrobotics.lib.mathematics.twodim.control
 
 import org.ghrobotics.lib.mathematics.twodim.geometry.Pose2d
+import org.ghrobotics.lib.mathematics.units.Time
+import org.ghrobotics.lib.mathematics.units.derivedunits.acceleration
+import org.ghrobotics.lib.mathematics.units.derivedunits.velocity
+import org.ghrobotics.lib.mathematics.units.meter
+import org.ghrobotics.lib.mathematics.units.millisecond
+import org.ghrobotics.lib.mathematics.units.radian
+import org.ghrobotics.lib.subsystems.drive.TrajectoryTrackerOutput
+import org.ghrobotics.lib.utils.DeltaTime
 
 /**
  * https://www.dis.uniroma1.it/~labrob/pub/papers/Ramsete01.pdf section 6, polar tracker
  */
-class PolarPostureStabilizer(val target: Pose2d) {
-    fun update(robotPose: Pose2d) {
-        // TODO
+class PolarPostureStabilizer(
+    val k1: Double,
+    val k2: Double,
+    val k3: Double
+) {
 
+    private var target: Pose2d = Pose2d()
+
+    private var deltaTimeController = DeltaTime()
+
+    var previousLinearVelocity = 0.meter.velocity
+    var previousAngularVelocity = 0.radian.velocity
+
+    fun reset(target: Pose2d) {
+        this.target = target
+        deltaTimeController.reset()
+        previousLinearVelocity = 0.meter.velocity
+        previousAngularVelocity = 0.radian.velocity
+    }
+
+    fun nextState(robotPose: Pose2d, currentTime: Time = System.currentTimeMillis().millisecond): TrajectoryTrackerOutput {
+        val deltaTime = deltaTimeController.updateTime(currentTime)
+
+        val robotRelativeToOrigin = robotPose inFrameOfReferenceOf target
+        val theta = robotPose.rotation.radian
+
+        val row = robotRelativeToOrigin.translation.norm.meter
+        val gamma = (Math.atan2(
+            robotRelativeToOrigin.translation.y.value,
+            robotRelativeToOrigin.translation.x.value
+        ) - theta + Math.PI).radian.value
+        val sigma = (gamma + theta).radian.value
+
+        val (linear, angular) =
+            (k1 * row * Math.cos(gamma)).meter.velocity to
+                (k2 * gamma + k1 * Math.sin(gamma) * Math.cos(gamma) / gamma * (gamma + k3 * sigma)).radian.velocity
+
+        println(linear.value)
+
+        return if (deltaTime.value <= 0) {
+            TrajectoryTrackerOutput(
+                linearVelocity = linear,
+                linearAcceleration = 0.meter.acceleration,
+                angularVelocity = angular,
+                angularAcceleration = 0.radian.acceleration
+            )
+        } else {
+            TrajectoryTrackerOutput(
+                linearVelocity = linear,
+                linearAcceleration = (linear - previousLinearVelocity) / deltaTime,
+                angularVelocity = angular,
+                angularAcceleration = (angular - previousAngularVelocity) / deltaTime
+            )
+        }
     }
 }
---
+++ src/test/kotlin/org/ghrobotics/lib/mathematics/twodim/control/PolarPostureStabilizerTest.kt
@@ -0,0 +1,80 @@
+package org.ghrobotics.lib.mathematics.twodim.control
+
+import org.ghrobotics.frc2019.Constants
+import org.ghrobotics.lib.mathematics.twodim.geometry.Pose2d
+import org.ghrobotics.lib.mathematics.units.*
+import org.ghrobotics.lib.simulation.SimDifferentialDrive
+import org.ghrobotics.lib.simulation.SimFalconMotor
+import org.junit.Test
+import org.knowm.xchart.SwingWrapper
+import org.knowm.xchart.XYChartBuilder
+import java.awt.Color
+import java.awt.Font
+
+class PolarPostureStabilizerTest {
+    @Test
+    fun testPostureStabilizer() {
+        val polarPostureStabilizer = PolarPostureStabilizer(1.0, 3.0, 2.0)
+
+        val drive = SimDifferentialDrive(
+            Constants.kDriveModel,
+            SimFalconMotor(0.meter),
+            SimFalconMotor(0.meter),
+            FeedForwardTracker(),
+            1.05
+        )
+
+        var currentTime = 0.second
+        val deltaTime = 20.millisecond
+
+        val xList = arrayListOf<Double>()
+        val yList = arrayListOf<Double>()
+
+
+        val target = Pose2d(20.feet, 5.feet, 0.degree)
+
+        polarPostureStabilizer.reset(target)
+
+        drive.robotPosition = Pose2d(10.0.feet, 10.feet, 30.degree)
+
+        while (currentTime < 12.second) {
+            currentTime += deltaTime
+            drive.setOutput(polarPostureStabilizer.nextState(drive.robotPosition, currentTime))
+            drive.update(deltaTime)
+
+            xList += drive.robotPosition.translation.x.feet
+            yList += drive.robotPosition.translation.y.feet
+        }
+
+        val chart = XYChartBuilder().width(1800).height(1520).title("Polar Posture Stabilizer")
+            .xAxisTitle("X").yAxisTitle("Y").build()
+
+        chart.styler.markerSize = 8
+        chart.styler.seriesColors = arrayOf(Color.ORANGE, Color(151, 60, 67))
+
+        chart.styler.chartTitleFont = Font("Kanit", 1, 40)
+        chart.styler.chartTitlePadding = 15
+
+        chart.styler.xAxisMin = 1.0
+        chart.styler.xAxisMax = 26.0
+        chart.styler.yAxisMin = 1.0
+        chart.styler.yAxisMax = 26.0
+
+        chart.styler.chartFontColor = Color.WHITE
+        chart.styler.axisTickLabelsColor = Color.WHITE
+
+        chart.styler.legendBackgroundColor = Color.GRAY
+
+        chart.styler.isPlotGridLinesVisible = true
+        chart.styler.isLegendVisible = true
+
+        chart.styler.plotGridLinesColor = Color.GRAY
+        chart.styler.chartBackgroundColor = Color.DARK_GRAY
+        chart.styler.plotBackgroundColor = Color.DARK_GRAY
+
+        chart.addSeries("Robot", xList.toDoubleArray(), yList.toDoubleArray())
+
+//        SwingWrapper(chart).displayChart()
+//        Thread.sleep(1000000)
+    }
+}
