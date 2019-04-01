19a16b199bc8bf9afa4ccfbe331dd150a4448ab7
ThePrimedTNT <13050203+ThePrimedTNT@users.noreply.github.com>
2019-03-31 20:52:58 -0400

    Dont continue climb if lidar is unplugged

--- src/main/kotlin/org/ghrobotics/frc2019/subsystems/climb/AutoClimbRoutines.kt
+++ src/main/kotlin/org/ghrobotics/frc2019/subsystems/climb/AutoClimbRoutines.kt
@@ -2,10 +2,7 @@ package org.ghrobotics.frc2019.subsystems.climb
 
 import org.ghrobotics.frc2019.subsystems.arm.ClosedLoopArmCommand
 import org.ghrobotics.frc2019.subsystems.drive.DriveSubsystem
-import org.ghrobotics.lib.commands.DelayCommand
-import org.ghrobotics.lib.commands.FalconCommand
-import org.ghrobotics.lib.commands.parallel
-import org.ghrobotics.lib.commands.sequential
+import org.ghrobotics.lib.commands.*
 import org.ghrobotics.lib.mathematics.units.degree
 import org.ghrobotics.lib.mathematics.units.inch
 import org.ghrobotics.lib.mathematics.units.millisecond
@@ -14,26 +11,33 @@ import org.ghrobotics.lib.utils.Source
 
 object AutoClimbRoutines {
 
-    private val group get() = sequential {
-        +parallel {
-            +ClimbWheelCommand(Source(0.75))
-            +ClosedLoopArmCommand(160.degree)
-        }.withExit { ClimbSubsystem.lidarRawAveraged < 500 }
+    private val group
+        get() = sequential {
+            +parallel {
+                +ClimbWheelCommand(Source(0.75))
+                +ClosedLoopArmCommand(160.degree)
+            }.withExit { ClimbSubsystem.lidarRawAveraged < 500 }
 
-        +ClimbWheelCommand(Source(0.5)).withTimeout(200.millisecond)
+            // Continue climb only if lidar is not faulty
+            +ConditionalCommand(
+                { ClimbSubsystem.lidarRawAveraged > 25 },
+                sequential {
+                    +ClimbWheelCommand(Source(0.5)).withTimeout(200.millisecond)
 
-        val resetBack = ResetWinchCommand(resetFront = false)
+                    val resetBack = ResetWinchCommand(resetFront = false)
 
-        +parallel {
-            +ClosedLoopArmCommand(105.degree)
-            +ClimbWheelCommand(Source(0.05))
-            +resetBack
-        }.withExit { resetBack.wrappedValue.isCompleted }
+                    +parallel {
+                        +ClosedLoopArmCommand(105.degree)
+                        +ClimbWheelCommand(Source(0.05))
+                        +resetBack
+                    }.withExit { resetBack.wrappedValue.isCompleted }
 
-        +ClimbWheelCommand(Source(1.0)).withExit { ClimbSubsystem.frontOnPlatform }.withTimeout(2.5.second)
-        +ResetWinchCommand(resetFront = true)
-        +DelayCommand(1.second)
-    }
+                    +ClimbWheelCommand(Source(1.0)).withExit { ClimbSubsystem.frontOnPlatform }.withTimeout(2.5.second)
+                    +ResetWinchCommand(resetFront = true)
+                    +DelayCommand(1.second)
+                }
+            )
+        }
 
     val autoL3Climb
         get() = sequential {
@@ -41,10 +45,11 @@ object AutoClimbRoutines {
                 22.7.inch,
                 19.7.inch
             )
+            val climbGroup = group
             +parallel {
                 +object : FalconCommand(DriveSubsystem) {
                     init {
-                        finishCondition += group.wrappedValue::isCompleted
+                        finishCondition += climbGroup.wrappedValue::isCompleted
                     }
 
                     override suspend fun execute() {
@@ -55,7 +60,7 @@ object AutoClimbRoutines {
                         }
                     }
                 }
-                +group
+                +climbGroup
             }
         }
 
@@ -65,17 +70,18 @@ object AutoClimbRoutines {
                 10.inch,
                 11.inch
             )
+            val climbGroup = group
             +parallel {
                 +object : FalconCommand(DriveSubsystem) {
                     init {
-                        finishCondition += group.wrappedValue::isCompleted
+                        finishCondition += climbGroup.wrappedValue::isCompleted
                     }
 
                     override suspend fun execute() {
                         DriveSubsystem.tankDrive(-.4, -.4)
                     }
                 }
-                +group
+                +climbGroup
             }
         }
 }
